5806e45fa629cfc3ab1da667b0832fd8b7da05c2
maryia-deriv <maryia@deriv.com>
2022-01-25 18:34:41 +0300

    Maryia/finilized the initial version of Form component

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { useFormik } from 'formik';
 import './Form.scss';
 
+type TFormRowsData = {
+    first_field_name: string;
+    second_field_name?: string;
+    label_text?: string;
+    first_field_placeholder?: string;
+    second_field_placeholder?: string;
+    input_type?: string;
+}[];
+
 type TFormValues = {
-    firstName: string;
-    lastName: string;
-    mobileNumber: string;
-    phoneNumber: string;
-    faxNumber: string;
+    first_name: string;
+    last_name: string;
+    mobile_number: string;
+    phone_number: string;
+    fax_number: string;
     email: string;
     company: string;
     job: string;
@@ -23,15 +32,75 @@ type TFormProps = {
     onDataSubmit: (v_card_string: string) => void;
 };
 
+const form_rows: TFormRowsData = [
+    {
+        first_field_name: 'first_name',
+        second_field_name: 'last_name',
+        label_text: 'Your Name:',
+        first_field_placeholder: 'First name',
+        second_field_placeholder: 'Last name',
+    },
+    {
+        first_field_name: 'mobile_number',
+        label_text: 'Contact:',
+        first_field_placeholder: 'Mobile',
+        input_type: 'tel',
+    },
+    {
+        first_field_name: 'phone_number',
+        second_field_name: 'fax_number',
+        first_field_placeholder: 'Phone',
+        second_field_placeholder: 'Fax',
+        input_type: 'tel',
+    },
+    {
+        first_field_name: 'email',
+        label_text: 'Email:',
+        first_field_placeholder: 'your@email.com',
+        input_type: 'email',
+    },
+    {
+        first_field_name: 'company',
+        second_field_name: 'job',
+        label_text: 'Company:',
+        first_field_placeholder: 'Company',
+        second_field_placeholder: 'Your Job',
+    },
+    {
+        first_field_name: 'street',
+        label_text: 'Street:',
+    },
+    {
+        first_field_name: 'city',
+        second_field_name: 'zip',
+        label_text: 'City:',
+        second_field_placeholder: 'ZIP',
+    },
+    {
+        first_field_name: 'state',
+        label_text: 'State:',
+    },
+    {
+        first_field_name: 'country',
+        label_text: 'Country:',
+    },
+    {
+        first_field_name: 'website',
+        label_text: 'Website:',
+        first_field_placeholder: 'www.your-website.com',
+        input_type: 'url',
+    },
+];
+
 export const Form: React.FC<TFormProps> = ({ onDataSubmit }) => {
     const convertValuesToVCardString = (values: TFormValues) => {
         onDataSubmit(`BEGIN:VCARD
         VERSION:3.0
-        N:${values.lastName};${values.firstName}
-        FN:${values.firstName} ${values.lastName}
+        N:${values.last_name};${values.first_name}
+        FN:${values.first_name} ${values.last_name}
         ORG:${values.company}
         TITLE:${values.job}
-        TEL;CELL:${values.mobileNumber};WORK:${values.phoneNumber};FAX:${values.faxNumber}
+        TEL;CELL:${values.mobile_number};WORK:${values.phone_number};FAX:${values.fax_number}
         EMAIL:${values.email}
         ADR:;;${values.street};${values.city};${values.state};${values.zip};${values.country}
         URL:${values.website}
@@ -41,11 +110,11 @@ export const Form: React.FC<TFormProps> = ({ onDataSubmit }) => {
 
     const formik = useFormik({
         initialValues: {
-            firstName: '',
-            lastName: '',
-            mobileNumber: '',
-            phoneNumber: '',
-            faxNumber: '',
+            first_name: '',
+            last_name: '',
+            mobile_number: '',
+            phone_number: '',
+            fax_number: '',
             email: '',
             company: '',
             job: '',
@@ -67,166 +136,77 @@ export const Form: React.FC<TFormProps> = ({ onDataSubmit }) => {
                 <p>VCARD QR Code</p>
             </h1>
             <form onSubmit={formik.handleSubmit}>
-                {/* 1 */}
-                <div className='form-group'>
-                    <label className='col-3' htmlFor='firstName'>
-                        Your Name:
-                    </label>
-                    <div className='col-9'>
-                        <div className='row'>
-                            <div className='col-6'>
-                                <input
-                                    id='firstName'
-                                    type='text'
-                                    placeholder='First name'
-                                    {...formik.getFieldProps('firstName')}
-                                />
+                {form_rows.map(
+                    (
+                        {
+                            first_field_name,
+                            second_field_name,
+                            label_text,
+                            first_field_placeholder,
+                            second_field_placeholder,
+                            input_type,
+                        },
+                        i
+                    ) => {
+                        const field_type = input_type ? input_type : 'text';
+                        const input_mode_type = input_type
+                            ? (input_type as keyof React.HTMLAttributes<HTMLInputElement>['inputMode'])
+                            : 'text';
+                        const first_placeholder = first_field_placeholder ? first_field_placeholder : '';
+                        const second_placeholder = second_field_placeholder ? second_field_placeholder : '';
+
+                        return (
+                            <div key={i} className='form-group'>
+                                {label_text ? (
+                                    <label className='col-3' htmlFor={first_field_name}>
+                                        {label_text}
+                                    </label>
+                                ) : null}
+                                <div className={'col-9'}>
+                                    {second_field_name ? (
+                                        <div
+                                            className={`row${
+                                                first_field_name === 'phone_number' ? ' align-right' : ''
+                                            }`}
+                                        >
+                                            <div
+                                                className={`col-${first_field_name === 'phone_number' ? '' : '6'}`}
+                                            >
+                                                <input
+                                                    id={first_field_name}
+                                                    type={field_type}
+                                                    inputMode={input_mode_type}
+                                                    placeholder={first_placeholder}
+                                                    {...formik.getFieldProps(first_field_name)}
+                                                />
+                                            </div>
+                                            <div
+                                                className={`col-${first_field_name === 'phone_number' ? '' : '6'}`}
+                                            >
+                                                <input
+                                                    id={second_field_name}
+                                                    type={field_type}
+                                                    inputMode={input_mode_type}
+                                                    placeholder={second_placeholder}
+                                                    {...formik.getFieldProps(second_field_name)}
+                                                />
+                                            </div>
+                                        </div>
+                                    ) : (
+                                        <input
+                                            id={first_field_name}
+                                            type={field_type}
+                                            inputMode={input_mode_type}
+                                            placeholder={first_placeholder}
+                                            {...formik.getFieldProps(first_field_name)}
+                                        />
+                                    )}
+                                </div>
                             </div>
-                            <div className='col-6'>
-                                <input
-                                    id='lastName'
-                                    type='text'
-                                    placeholder='Last name'
-                                    {...formik.getFieldProps('lastName')}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* 2 */}
-                <div className='form-group'>
-                    <label className='col-3' htmlFor='mobileNumber'>
-                        Contact:
-                    </label>
-                    <div className='col-9'>
-                        <input
-                            id='mobileNumber'
-                            type='tel'
-                            inputMode='tel'
-                            placeholder='Mobile'
-                            {...formik.getFieldProps('mobileNumber')}
-                        />
-                    </div>
-                </div>
-                {/* 3 */}
-                <div className='form-group'>
-                    <div className='col-10'>
-                        <div className='row align-right'>
-                            <div>
-                                <input
-                                    id='phoneNumber'
-                                    type='tel'
-                                    inputMode='tel'
-                                    placeholder='Phone'
-                                    {...formik.getFieldProps('phoneNumber')}
-                                />
-                            </div>
-                            <div>
-                                <input
-                                    id='faxNumber'
-                                    type='tel'
-                                    inputMode='tel'
-                                    placeholder='Fax'
-                                    {...formik.getFieldProps('faxNumber')}
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* 4 */}
-                <div className='form-group'>
-                    <label className='col-3' htmlFor='email'>
-                        Email:
-                    </label>
-                    <div className='col-9'>
-                        <input
-                            id='email'
-                            type='email'
-                            inputMode='email'
-                            placeholder='your@email.com'
-                            {...formik.getFieldProps('email')}
-                        />
-                    </div>
-                </div>
-                {/* 5 */}
-                <div className='form-group'>
-                    <label className='col-3' htmlFor='company'>
-                        Company:
-                    </label>
-                    <div className='col-9'>
-                        <div className='row'>
-                            <div className='col-6'>
-                                <input
-                                    id='company'
-                                    type='text'
-                                    placeholder='Company'
-                                    {...formik.getFieldProps('company')}
-                                />
-                            </div>
-                            <div className='col-6'>
-                                <input id='job' type='text' placeholder='Your Job' {...formik.getFieldProps('job')} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* 6 */}
-                <div className='form-group'>
-                    <label className='col-3' htmlFor='street'>
-                        Street:
-                    </label>
-                    <div className='col-9'>
-                        <input id='street' type='text' {...formik.getFieldProps('street')} />
-                    </div>
-                </div>
-                {/* 7 */}
-                <div className='form-group'>
-                    <label className='col-3' htmlFor='city'>
-                        City:
-                    </label>
-                    <div className='col-9'>
-                        <div className='row'>
-                            <div className='col-5'>
-                                <input id='city' type='text' {...formik.getFieldProps('city')} />
-                            </div>
-                            <div className='col-5'>
-                                <input id='zip' type='text' placeholder='ZIP' {...formik.getFieldProps('zip')} />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* 8 */}
-                <div className='form-group'>
-                    <label className='col-3' htmlFor='state'>
-                        State:
-                    </label>
-                    <div className='col-9'>
-                        <input id='state' type='text' {...formik.getFieldProps('state')} />
-                    </div>
-                </div>
-                {/* 9 */}
-                <div className='form-group'>
-                    <label className='col-3' htmlFor='country'>
-                        Country:
-                    </label>
-                    <div className='col-9'>
-                        <input id='country' type='text' {...formik.getFieldProps('country')} />
-                    </div>
-                </div>
-                {/* 10 */}
-                <div className='form-group'>
-                    <label className='col-3' htmlFor='website'>
-                        Website:
-                    </label>
-                    <div className='col-9'>
-                        <input
-                            id='website'
-                            type='url'
-                            inputMode='url'
-                            placeholder='www.your-website.com'
-                            {...formik.getFieldProps('website')}
-                        />
-                    </div>
-                </div>
+                        );
+                    }
+                )}
+
                 <button type='submit'> Generate QR Code </button>
             </form>
         </div>

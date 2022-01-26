1552787d89d0606de15dbbb75acdd5e67f46149d
mitra-fs <mitra@firstsource.tech>
2022-01-26 17:07:43 +0330

    Cleanup

--- src/components/QRCodeGenerator.tsx
+++ src/components/QRCodeGenerator.tsx
@@ -1,36 +1,32 @@
 import React, { useEffect, useState } from 'react';
 
 interface Props {
-    firstName: string;
-    lastName: string;
-    email: string;
+    data: string;
 }
 
-export const QRCodeGenerator: React.FC<Props> = ({ firstName, lastName, email }: Props) => {
+export const QRCodeGenerator: React.FC<Props> = ({ data }: Props) => {
     const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState<Response>();
+    const [responseData, setResponseData] = useState<Response>();
     const [hasError, setHasError] = useState<unknown>();
 
-    const generateQRCode = async (data: string) => {
+    const fetchQRCode = async () => {
         setIsLoading(true);
         try {
-            const response = await fetch(`https://qrcode-monkey.p.rapidapi.com/qr/custom?data=${data}`, {
+            const response = await fetch(`https://${process.env.API_HOST}/qr/custom?data=${data}`, {
                 method: 'GET',
                 headers: {
-                    'x-rapidapi-host': 'qrcode-monkey.p.rapidapi.com',
+                    'x-rapidapi-host': `${process.env.API_HOST}`,
                     'x-rapidapi-key': `${process.env.API_KEY}`,
                 },
             });
 
-            setData(response);
+            setResponseData(response);
             setIsLoading(false);
         } catch (error) {
             setHasError(error);
             setIsLoading(false);
         }
     };
-
-    const fetchQRCode = () => generateQRCode(`${firstName}\n${lastName}\n${email}`);
 
     useEffect(() => {
         fetchQRCode();
@@ -40,8 +36,8 @@ export const QRCodeGenerator: React.FC<Props> = ({ firstName, lastName, email }:
 
     if (hasError) return <button onClick={() => fetchQRCode()}> Try again </button>;
 
-    if (!data?.ok) return <button onClick={() => fetchQRCode()}> Try again x</button>;
+    if (!responseData?.ok) return <button onClick={() => fetchQRCode()}> Try again x</button>;
 
     // Todo: It should display the QR Code image here.
-    return <p>{data?.status}</p>;
+    return <p>{responseData?.status}</p>;
 };

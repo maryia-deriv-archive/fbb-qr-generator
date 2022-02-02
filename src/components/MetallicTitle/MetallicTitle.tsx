2a3b45fffa5fde0ad91af82762a7f57e81c1cfd2
maryia-deriv <maryia@deriv.com>
2022-02-02 14:41:47 +0300

    Maryia/changed a comment

--- src/components/MetallicTitle/MetallicTitle.tsx
+++ src/components/MetallicTitle/MetallicTitle.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
 import './MetallicTitle.scss';
 
-export const MetallicTitle: React.FC<{className?: string}> = ({ children, className }) => {
+// it's a beautifully styled wrapper that takes a title from children and displays it on a plate with screws (designed by pavel-latyshou-deriv):
+export const MetallicTitle: React.FC<{ className?: string }> = ({ children, className }) => {
     return (
         <>
-            {/* beautifully styled title on a plate with screws: */}
-                <div className={className ? className : 'screw_left'}>
-                    <div className='dot_left'>
-                        <div className='dot_minus'></div>
-                        <div className='dot_plus'></div>
-                    </div>
-                    <div className='dot_left'>
-                        <div className='dot_minus'></div>
-                        <div className='dot_plus'></div>
-                    </div>
+            <div className={className ? className : 'screw_left'}>
+                <div className='dot_left'>
+                    <div className='dot_minus'></div>
+                    <div className='dot_plus'></div>
                 </div>
-                {children}
-                <div className={className ? className : 'screw_left'}>
-                    <div className='dot_left'>
-                        <div className='dot_minus'></div>
-                        <div className='dot_plus'></div>
-                    </div>
-                    <div className='dot_left'>
-                        <div className='dot_minus'></div>
-                        <div className='dot_plus'></div>
-                    </div>
+                <div className='dot_left'>
+                    <div className='dot_minus'></div>
+                    <div className='dot_plus'></div>
                 </div>
+            </div>
+            {children}
+            <div className={className ? className : 'screw_left'}>
+                <div className='dot_left'>
+                    <div className='dot_minus'></div>
+                    <div className='dot_plus'></div>
+                </div>
+                <div className='dot_left'>
+                    <div className='dot_minus'></div>
+                    <div className='dot_plus'></div>
+                </div>
+            </div>
         </>
     );
 };
